64cae37ce62bff7d7564763894fbd768b471bfa9
Jos10-30ue <josue.amaya2028@hilasal.superate.org>
2026-06-29 20:15:20 -0600

    Parte de Backend

--- backend1.py
+++ backend1.py
@@ -1,4 +1,39 @@
-def guardar_ticket(nombre, problema):
-    print("nombre:", nombre)
-    print("problema:", problema)
+from tkinter import messagebox
+
+
+#Creando los tickets.
+
+tickets = []
+contador = 1
+
+def crear_new_ticket (user, ID , issue):
+    global contador 
+
+    if user == "" or ID == "" or issue == "":
+        messagebox.showerror("Campos Incomletos", "Debe completar todos los campos") 
+        return
+
+    datos = {
+        "Ticket": contador,
+        "ID" : ID,
+        "User" : user,
+        "Issue": issue
+    }
+
+    tickets.append (datos)
+    print(tickets)
+    contador += 1
+
+    messagebox.showinfo("EXITO" , "Ticket creado correctamente")
+
+def obtener_tickets():
+    return tickets
+
+
+    
+
+    
+
+
+
 
